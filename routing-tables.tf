fac55d388e1e269c8c2feb6da0b62ab4b4c831e4
akshaykumart <106388941+akshaykumart@users.noreply.github.com>
2022-10-09 15:18:12 +0530

    Update routing-tables.tf
    NAT gateway changes

--- routing-tables.tf
+++ routing-tables.tf
@@ -30,7 +30,7 @@ resource "aws_route_table" "private2" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.gw2.id
+    gateway_id = aws_nat_gateway.gw1.id
   }
 
   tags = {
